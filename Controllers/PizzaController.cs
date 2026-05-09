37dc3496f20c686a0cac6dc782102a73f2a2be27
Bathandwa <bmap750@gmail.com>
2026-05-09 13:10:05 +0200

    Initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,20 @@
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+    }
+
+    // GET all action
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+}
