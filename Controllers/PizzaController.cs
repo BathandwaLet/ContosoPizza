725b4b8b4c96b33c4748b304b459ad21bfa99394
Bathandwa <bmap750@gmail.com>
2026-05-09 14:27:28 +0200

    bug: fixed return statement

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -33,7 +33,7 @@ public class PizzaController : ControllerBase
     public IActionResult Create(Pizza pizza)
     {
         PizzaService.Add(pizza);
-        return CreatedAction(nameof(Get), new { id = pizza.Id }, pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
     }
     
     // PUT action
